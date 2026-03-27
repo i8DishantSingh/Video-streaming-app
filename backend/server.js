58334d757a2690e641369c137e4eee620506b7c6
i8DishantSingh <professional.id.dishant@gmail.com>
2026-03-27 22:42:35 +0530

    Create initial auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 import express from 'express';
-
+import authRoutes from './routes/auth.route.js';
 const app = express();
 
+app.use('/api/v1/auth', authRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
-})
 
 app.listen(5000, () => {
     console.log('Server is running of port 3000');
