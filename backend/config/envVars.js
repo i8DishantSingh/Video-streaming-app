e347341c70c1bb2d38fbbd931d9e91e4cc27edc7
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-10 22:52:03 +0530

    Updated environment variables

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -6,4 +6,5 @@ export const ENV_VARS = {
     MONGO_URI : process.env.MONGO_URI,
     PORT : process.env.PORT || 5000,
     JWT_SECRET : process.env.JWT_SECRET,
+    TMDB_API_KEY : process.env.TMDB_API_KEY
 }
