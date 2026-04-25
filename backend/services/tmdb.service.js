6117a875d9bf70e32a94aee55548eb14887b354c
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-25 15:47:57 +0530

    Updated tmdb.services file

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -1,18 +1,16 @@
 import axios from "axios";
+import { ENV_VARS } from "../config/envVars.js";
 
-
-export const fetchMovies = async (url) => {
-  const url =
-    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+export const fetchFromTMDB = async (url) => {
   const options = {
-    method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: "Bearer" + ENV_VARS.TMDB_API_KEY,
+      Authorization: "Bearer " + ENV_VARS.TMDB_API_KEY,
     },
   };
-
   const response = await axios(url, options);
-
+  if (response.status !== 200) {
+    throw new Error("Failed to fetch data from TMDB" + response.statusText);
+  }
   return response.data;
 };
