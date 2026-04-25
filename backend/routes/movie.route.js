09a96ab64b54a7e0e8dce4c13f34ee2ef3679a1c
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-25 15:49:56 +0530

    New routes created

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import { getTrendingMovie, getMovieTrailer } from '../controllers/movie.controller.js';
-
+import express from "express";
+import {
+  getTrendingMovie,
+  getMovieTrailer,
+  getMovieDetails,
+} from "../controllers/movie.controller.js";
 
 const router = express.Router();
 
+router.get("/trending", getTrendingMovie);
+router.get("/:id/trailer", getMovieTrailer);
+router.get("/:id/details", getMovieDetails);
 
-router.get('/trending', getTrendingMovie);
-router.get('/:id/trailer', getMovieTrailer);
-
-export default router; 
+export default router;
