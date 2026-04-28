868f2c1b445161217023f3cfaeb03114a4550067
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-28 23:05:46 +0530

    Updated server file

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,22 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvShowRoutes from "./routes/tvShow.route.js";
+import protectRoute from "./middleware/protectRoute.js";
+import cookieParser from "cookie-parser";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cookieParser());
 
 const PORT = ENV_VARS.PORT;
 console.log("Database URL: ", ENV_VARS.MONGO_URI);
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tvShow", tvShowRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tvShow", protectRoute, tvShowRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
