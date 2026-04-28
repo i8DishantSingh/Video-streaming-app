72ce48b9b47516bcd694ef5421b198f6751290a4
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-28 23:07:08 +0530

    Minor changes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,20 +1,16 @@
-import jwt from 'jsonwebtoken'
-import { ENV_VARS } from '../config/envVars.js'
-
+import jwt from "jsonwebtoken";
+import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-    const token = jwt.sign({ id: userId }, ENV_VARS.JWT_SECRET, { expiresIn: '15d' })
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        maxAge: 15 * 24 * 60 * 60 * 1000,
-        sameSite: 'strict',
-        secure: process.env.NODE_ENV !== 'development',
-    })
+  const token = jwt.sign({ id: userId }, ENV_VARS.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
 
-    return token
-} 
-
- 
-
-
-
+  return token;
+};
