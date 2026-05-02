80c858a3005b9eea7747cc82c17b1324bd638a8a
i8DishantSingh <professional.id.dishant@gmail.com>
2026-05-02 15:24:02 +0530

    Built searchRoute

--- backend/routes/search.route.js
+++ backend/routes/search.route.js
@@ -3,11 +3,16 @@ import {
   searchMovie,
   searchPerson,
   searchTv,
+  getSearchHisory,
+  removeItemFromSearchHistory,
 } from "../controllers/search.controller.js";
 const router = express.Router();
 
 router.get("/person/:query", searchPerson);
 router.get("/movie/:query", searchMovie);
 router.get("/tv/:query", searchTv);
+
+router.get("/history", getSearchHisory);
+router.delete("/history/:id", removeItemFromSearchHistory);
 
 export default router;
